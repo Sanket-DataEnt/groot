ee47dd036ee1f6f45f1839a590772a2aab773b03
Sanket Maheshwari <sanketmaheshwari96@gmail.com>
2023-07-05 00:47:01 +0530

    Update custom_resnet.py
    Added complete custom resnet code

--- CIFAR/custom_resnet.py
+++ CIFAR/custom_resnet.py
@@ -33,29 +33,37 @@ def custom_conv_layer(in_channels,
 class Net(nn.Module):
   def __init__(self, normtype):
       super(Net, self).__init__()
-      # prep layer - PrepLayer - Conv 3x3 s1, p1) >> BN >> RELU [64k]
+      # prep layer
       self.prep_layer = custom_conv_layer(3, 64, False, 'batch')
-      # Layer1 -
-      #   X = Conv 3x3 (s1, p1) >> MaxPool2D >> BN >> RELU [128k]
-      #   R1 = ResBlock( (Conv-BN-ReLU-Conv-BN-ReLU))(X) [128k] 
-      #   Add(X, R1)
+      # layer 1
       self.layer1_x = custom_conv_layer(64, 128, True, 'batch')
       self.layer1_r1 = nn.Sequential(
-        custom_conv_layer(128, 128, False, 'batch')
+        custom_conv_layer(128, 128, False, 'batch'),
         custom_conv_layer(128, 128, False, 'batch')
       )
-      # Layer 2 -
-          # Conv 3x3 [256k]
-          # MaxPooling2D
-          # BN
-          # ReLU
+      # layer 2
       self.layer2 = custom_conv_layer(128, 256, True, 'batch')
-      # Layer 3 -
-      #   X = Conv 3x3 (s1, p1) >> MaxPool2D >> BN >> RELU [512k]
-      #   R2 = ResBlock( (Conv-BN-ReLU-Conv-BN-ReLU))(X) [512k]
-      #   Add(X, R2)
+      # Layer 3
       self.layer3_x = custom_conv_layer(256, 512, True, 'batch')
+      self.layer3_r3 = nn.Sequential(
+        custom_conv_layer(512, 512, False, 'batch'),
+        custom_conv_layer(512, 512, False, 'batch')       
+      )
+      # MaxPooling with Kernel Size 4
+      self.pool = nn.MaxPool2d(4, 4)
+      # FC Layer 
+      self.fc = nn.Linear(512, 10)
 
-
-
-
+  def forward(self, x):
+    x = self.prep_layer(x)
+    x1 = self.layer1_x(x)
+    r1 = self.layer1_r1(x1)
+    x = x1 + r1
+    x = self.layer2(x)
+    x3 = self.layer3_x(x)
+    r3 = self.layer3_r3(x3)
+    x = x3 + r3
+    x = self.pool(x)
+    x = x.view(-1, 512)
+    x = self.fc(x)
+    return x
