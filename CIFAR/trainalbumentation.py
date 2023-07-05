94bab29d1fa8bd22ff67d08d2c11e3ede22b639f
Sanket Maheshwari <sanketmaheshwari96@gmail.com>
2023-07-05 17:22:38 +0530

    Update trainalbumentation.py

--- CIFAR/trainalbumentation.py
+++ CIFAR/trainalbumentation.py
@@ -15,7 +15,7 @@ class TrainAlbumentation():
       #  CoarseDropout(max_holes = 1, max_height=16, max_width=16, min_holes = 1, min_height=16, min_width=16, fill_value=[255*0.485,255*0.456,255*0.406], mask_fill_value = None),
        RandomCrop(32, 32, always_apply=False, p=1.0),
        CenterCrop(32, 32, always_apply=False, p=1.0),
-       Cutout (num_holes=1, max_h_size=8, max_w_size=8, fill_value=0, always_apply=False, p=0.5),
+       Cutout (num_holes=1, max_h_size=8, max_w_size=8, fill_value=[255*0.485,255*0.456,255*0.406], always_apply=False, p=0.5),
        Normalize(
         mean=[0.485,0.456,0.406],
         std=[0.229,0.224,0.225],
