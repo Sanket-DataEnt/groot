30286765127303f357078bf00756029afb4aa16f
sanket <sanketmaheshwari96@gmail.com>
2023-07-29 08:07:58 +0530

    added grad_cam code

--- CIFAR/utilities/utils.py
+++ CIFAR/utilities/utils.py
@@ -8,6 +8,13 @@ from albumentations.augmentations.geometric.transforms import PadIfNeeded
 from albumentations.augmentations.crops.transforms import CenterCrop
 from albumentations.augmentations.dropout.cutout import Cutout
 import matplotlib.pyplot as plt
+from pytorch_grad_cam import GradCAM
+from pytorch_grad_cam.utils.model_targets import ClassifierOutputTarget
+from pytorch_grad_cam.utils.image import show_cam_on_image
+
+import warnings
+warnings.filterwarnings("ignore", category=UserWarning, module="matplotlib.image")
+
 
 def get_lr(optimizer):
   for param_group in optimizer.param_groups:
@@ -207,3 +214,54 @@ def correct_classified_images(model, testloader, device, classes):
     frame1.axes.yaxis.set_ticklabels([])
     plt.imshow(np.transpose(((data[missed[i]].cpu()/2)+0.5).numpy(),(1,2,0)))
     plt.ylabel("GT:"+str(classes[target1[missed[i]]])+'\nPred:'+str(classes[predicted1[missed[i]]]))
+
+def grad_cam(net, img, targ, image_tensor):
+  target_layers = [net.layer4[-1]]
+  # Construct the CAM object once, and then re-use it on many images:
+  cam = GradCAM(model=net, target_layers=target_layers, use_cuda=True)
+  # We have to specify the target we want to generate
+  # the Class Activation Maps for.
+  # If targets is None, the highest scoring category
+  # will be used for every image in the batch.
+  # Here we use ClassifierOutputTarget, but you can define your own custom targets
+  # That are, for example, combinations of categories, or specific outputs in a non standard model.
+  targets = [ClassifierOutputTarget(targ[img])]
+
+  # You can also pass aug_smooth=True and eigen_smooth=True, to apply smoothing.
+  grayscale_cam = cam(input_tensor=image_tensor[img].unsqueeze(0), targets=targets)
+
+  # In this example grayscale_cam has only one image in the batch:
+  grayscale_cam = grayscale_cam[0, :]
+
+  input_image = image_tensor[img].permute(1, 2, 0)
+  input_image_normalized = (input_image - input_image.min()) / (input_image.max() - input_image.min())
+
+  # Convert the normalized image tensor to NumPy array and change data type to np.float32
+  input_image_np = input_image_normalized.cpu().numpy().astype(np.float32)
+
+  visualization = show_cam_on_image(input_image_np, grayscale_cam, use_rgb=True)
+  return input_image_np, visualization
+
+def visualize_gradcam(net, image_tensor, targ, pred, classes):
+  plt.subplots_adjust(left=0.1,
+                    bottom=0.1,
+                    right=0.9,
+                    top=0.9,
+                    wspace=0.2,
+                    hspace=0.2)
+  maxi = 10
+  # Set the figure size to adjust the size of the displayed images
+  plt.figure(figsize=(10, 2 * maxi))  # Adjust the width and height as needed
+  for img in range(maxi):
+    input_image_np,visualization=grad_cam(net, img, targ, image_tensor)
+    plt.subplot(maxi,2,2*img+1)
+    frame1 = plt.gca()
+    frame1.axes.xaxis.set_ticklabels([])
+    frame1.axes.yaxis.set_ticklabels([])
+    plt.imshow(input_image_np)
+    plt.subplot(maxi,2,2*img+2)
+    plt.ylabel("GT:"+str(classes[targ[img]])+'\nPred:'+str(classes[pred[img]]))
+    frame1 = plt.gca()
+    frame1.axes.xaxis.set_ticklabels([])
+    frame1.axes.yaxis.set_ticklabels([])
+    plt.imshow(visualization)
