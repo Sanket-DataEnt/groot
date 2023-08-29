544d512bbdb2dede3da45a4e17da238df08ba504
Sanket Maheshwari <sanketmaheshwari96@gmail.com>
2023-08-29 16:58:35 +0530

    updated tensorboard import

--- Transformer/train.py
+++ Transformer/train.py
@@ -14,14 +14,17 @@ import os
 from pathlib import Path
 
 # Huggingface datasets and tokenizers
-from datasets import load_dataset 
+from dataset import load_dataset 
 from tokenizers import Tokenizer 
 from tokenizers.models import WordLevel 
 from tokenizers.trainers import WordLevelTrainer 
 from tokenizers.pre_tokenizers import Whitespace
 
 import torchmetrics
-from torch.utils.tensorboard import Summarywriter
+# from torch.utils.tensorboard import Summarywriter
+from tensorboardX import SummaryWriter
+
+
 
 
 def greedy_decode (model, source, source_mask, tokenizer_sc, tokenizer_tgt, max_len, device):
