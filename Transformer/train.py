fc49c7718fbd981f43db52f9230256c1ecd2bf44
sanket <sanketmaheshwari96@gmail.com>
2023-08-30 18:05:01 +0530

    updated train file to tell epoch details explicitly

--- Transformer/train.py
+++ Transformer/train.py
@@ -214,7 +214,7 @@ def train_model(config):
     for epoch in range(initial_epoch, config['num_epochs']) :
         torch.cuda.empty_cache()
         model.train()
-        batch_iterator = tqdm(train_dataloader, desc=f"Processing Epoch (epoch: 02d)")
+        batch_iterator = tqdm(train_dataloader, desc=f"Processing Epoch {epoch:02d}")
         for batch in batch_iterator:
 
             encoder_input = batch['encoder_input'].to(device) # (b, seq_1en)
